f2b8acbc4e6c8a259c59f8768844aed441ee349d
Dmitry Shishkin <4144442@bk.ru>
2022-08-17 22:11:37 +0300

    Задание 4. Parentheses. Решение в JAVA-коде

--- src/tests/SimpleTests.java
+++ src/tests/SimpleTests.java
@@ -1,65 +1,21 @@
 package tests;
 
-import main.Array;
-import org.junit.After;
+import main.Parentheses;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 public class SimpleTests {
 
-    private ByteArrayOutputStream bos;
-    private PrintStream ps;
-
-    @Before
-    public void changeOut() {
-        ps = System.out;
-        bos = new ByteArrayOutputStream();
-        PrintStream stream = new PrintStream(bos);
-        System.setOut(stream);
-    }
-
     @Test
     public void testSimple1() {
-        Array.multiplesOfThree(new int[]{1, 3, 2, 6, 4, 9});
-        String expected = "3" + System.lineSeparator() + "6" + System.lineSeparator() + "9" + System.lineSeparator();
-        Assert.assertEquals(expected, bos.toString());
+        boolean actual = Parentheses.isValid("[((())()(())]]");
+        Assert.assertFalse(actual);
     }
 
     @Test
     public void testSimple2() {
-        Array.multiplesOfThree(null);
-        String expected = "";
-        Assert.assertEquals(expected, bos.toString());
-    }
-
-    @Test
-    public void testSimple3() {
-        Array.multiplesOfThree(new int[]{1});
-        String expected = "";
-        Assert.assertEquals(expected, bos.toString());
-    }
-
-    @Test
-    public void testSimple4() {
-        Array.multiplesOfThree(new int[]{});
-        String expected = "";
-        Assert.assertEquals(expected, bos.toString());
-    }
-
-    @Test
-    public void testSimple5() {
-        Array.multiplesOfThree(new int[]{9});
-        String expected = "9" + System.lineSeparator();
-        Assert.assertEquals(expected, bos.toString());
-    }
-
-    @After
-    public void returnOut() {
-        System.setOut(ps);
+        boolean actual = Parentheses.isValid("([([])])");
+        Assert.assertTrue(actual);
     }
 }
 
